a1a010731aa019cae3d9917b2445a2b20a3e2367
ESANAV2 <alvaro.navarro@atradius.com>
2024-12-03 10:31:05 +0100

    upload exercise

--- src/main/java/com/advent/Exercise3.java
+++ src/main/java/com/advent/Exercise3.java
@@ -1,8 +1,20 @@
 package com.advent;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class Exercise3 {
 
-    public static int multiplicationsSum(String intput){
-        return 0;
+    public static int multiplicationsSum(String input){
+        var scanner = new Scanner(input);
+
+        return scanner.findAll(Pattern.compile("mul\\((\\d+),(\\d+)\\)"))
+                .map(mr -> {
+                    var a = Integer.parseInt(mr.group(1));
+                    var b = Integer.parseInt(mr.group(2));
+                    return a * b;
+                }).reduce(0, Integer::sum);
     }
 }
--- src/test/java/com/advent/Exercise3Test.java
+++ src/test/java/com/advent/Exercise3Test.java
@@ -3,12 +3,26 @@ package com.advent;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 class Exercise3Test {
 
     @Test
     void multiplicationsSumTest(){
-        var result = Exercise3.multiplicationsSum("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+        String input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+        var result = Exercise3.multiplicationsSum(input);
         //161 (2*4 + 5*5 + 11*8 + 8*5)
         Assertions.assertEquals(161,result);
+    }
+
+    @Test
+    void multiplicationsSumLargeExampleTest() throws IOException {
+        var program = Files.readString(Path.of("src/test/resources/exercise3/program.txt"));
+        var result = Exercise3.multiplicationsSum(program);
+        System.out.println("Multiplications added: " + result);
+        Assertions.assertTrue(result >0);
+        //190604937 That's the right answer!
     }
 }
