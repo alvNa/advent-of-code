19350c8e524cecc02d93d35f724d3a67c19fcea0
juanantonio.lopezvisa@amaris.com <alvNa@users.noreply.github.com>
2024-12-10 08:57:03 +0100

    fix solution

--- src/test/java/com/advent/Exercise9Test.java
+++ src/test/java/com/advent/Exercise9Test.java
@@ -18,7 +18,14 @@ class Exercise9Test {
     }
 
     @Test
-    void fileSystemCheckSumRangeTest() {
+    void fileSystemCheckSumRange1Test() {
+        var diskMap = "233313312141413140211";
+        var result = Exercise9b.fileSystemCheckSum(diskMap);
+        Assertions.assertEquals(4024, result);
+    }
+
+    @Test
+    void fileSystemCheckSumRange2Test() {
         var diskMap = "2333133121414131402332233";
         var result = Exercise9b.fileSystemCheckSum(diskMap);
         Assertions.assertEquals(4024, result);
@@ -26,14 +33,14 @@ class Exercise9Test {
 
     @Test
     void fileSystemCheckSumLargeTest() throws IOException {
-        //var diskMap = "2333133121414131402";
         var diskMap = Files.readString(Path.of("src/test/resources/exercise9/disk-map-large.txt"));
         var result = Exercise9b.fileSystemCheckSum(diskMap);
         System.out.println("CheckSum: " + result);
         //CheckSum: 181189696 That's not the right answer; your answer is too low
         //CheckSum: 282055995 That's not the right answer; your answer is too low.
-
+        // 6356833654075 -> OK
         Assertions.assertTrue(result>0);
+        Assertions.assertEquals(6356833654075L, result);
     }
 
 }
