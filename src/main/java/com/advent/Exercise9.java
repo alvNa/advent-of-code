ae75cf11d6da9ce5529bafd9446f33053a46e54f
ESANAV2 <alvaro.navarro@atradius.com>
2024-12-10 09:02:18 +0100

    update part1

--- src/main/java/com/advent/Exercise9.java
+++ src/main/java/com/advent/Exercise9.java
@@ -1,120 +1,84 @@
 package com.advent;
 
-import java.nio.charset.Charset;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Exercise9 {
 
-    public static final char FREE_POSITION = '.';
+    public static final String FREE_POSITION = ".";
 
-    public static int fileSystemCheckSum(String diskMap){
+    public static long fileSystemCheckSum(String diskMap){
         var disk = decodeDiskMap(diskMap);
-        //System.out.println(disk);
+//        System.out.println(disk);
         var compressedDisk = compressDiskMap(disk);
-        System.out.println(compressedDisk);
+//        System.out.println(compressedDisk);
         return calculateCheckSum(compressedDisk);
     }
 
-    private static int calculateCheckSum(char[] compressedDisk) {
-        int sum = 0;
+    private static long calculateCheckSum(List<String> compressedDisk) {
+        long sum = 0;
         boolean finished = false;
         int i = 0;
-        while (i<compressedDisk.length && !finished){
+        while (i<compressedDisk.size() && !finished){
 
-            if (compressedDisk[i] == FREE_POSITION){
+            if (Objects.equals(compressedDisk.get(i), FREE_POSITION)){
                 finished=true;
             }
             else {
-                char c = compressedDisk[i];
-//                System.out.println("c: " + c);
-                var num = Integer.parseInt(String.valueOf(c));
-                sum+=num*i;
+                var num = Long.parseLong(compressedDisk.get(i));
+                long numValue = i * num;
+                sum += numValue;
             }
             i++;
         }
         return sum;
     }
 
-    private static char[] compressDiskMap(char[] disk) {
-        var compressDisk = Arrays.copyOf(disk,disk.length);
+    private static List<String> compressDiskMap(List<String> disk) {
+        var compressDisk = new ArrayList<>(disk);
 
         var indexLeft = 0;
-        var indexRight = disk.length-1;
+        var indexRight = disk.size()-1;
 
         while (indexLeft < indexRight){
 
-            while (compressDisk[indexLeft] != FREE_POSITION){
+            while (!Objects.equals(compressDisk.get(indexLeft), FREE_POSITION)){
                 indexLeft++;
             }
 
-            while (compressDisk[indexRight] == FREE_POSITION){
+            while (Objects.equals(compressDisk.get(indexRight), FREE_POSITION)){
                 indexRight--;
             }
 
             if (indexLeft < indexRight) {
-                char number = compressDisk[indexRight];
-                compressDisk[indexLeft] = number;
-                compressDisk[indexRight] = FREE_POSITION;
+                String number = compressDisk.get(indexRight);
+                compressDisk.remove(indexLeft);
+                compressDisk.add(indexLeft, number);
+                compressDisk.remove(indexRight);
+                compressDisk.add(indexRight, FREE_POSITION);
             }
         }
 
         return compressDisk;
     }
 
-    private static char[] decodeDiskMap(String diskMap) {
-        int size = calculateSize(diskMap);
-        char[] diskArray = new char[size];
-        int currentPosition=0;
-        Integer idNumber=0;
+    private static List<String> decodeDiskMap(String diskMap) {
+        var diskArray = new ArrayList<String>();
+        int idNumber=0;
         for (int i=0;i<diskMap.length();i++){
             int num = Integer.parseInt(String.valueOf(diskMap.charAt(i)));
 
-            if (i %2 == 0){
-//                if (idNumber.toString().length()>1){
-                    for (int j=0;j<num; j++){
-                        for(char c: idNumber.toString().toCharArray()){
-                            diskArray[currentPosition++] = c;
-                        }
-                    }
-//                }
-//                else{
-//                    var c = Character.forDigit(idNumber,10);
-//                    Arrays.fill(diskArray, currentPosition, currentPosition+num, c);
-//                    currentPosition+=num;
-//                }
-
-                idNumber++;
+            for (int j=0;j<num; j++){
+                String value = (i %2 == 0) ? Integer.toString(idNumber):FREE_POSITION;
+                diskArray.add(value);
             }
-            else {
-                Arrays.fill(diskArray, currentPosition, currentPosition+num, FREE_POSITION);
-                currentPosition+=num;
+
+            if (i %2 == 0){
+                idNumber++;
             }
         }
 
         return diskArray;
     }
-
-    //2333133121414131402
-    private static int calculateSize(String diskMap) {
-        int size = 0;
-        var idNumber=0;
-        for (int i=0;i<diskMap.length();i++){
-            int numRepeat = Integer.parseInt(String.valueOf(diskMap.charAt(i)));
-            if (i %2 == 0){
-                var numDigits = String.valueOf(idNumber).length();
-                size+= (numRepeat * numDigits);
-                idNumber++;
-            }
-            else{
-                size+= numRepeat;
-            }
-//            if (String.valueOf(num).length()>1){
-//                size+= num * String.valueOf(num).length();
-//            }
-//            size+= num;
-        }
-        return size;
-    }
-
-
 }
--- src/test/java/com/advent/Exercise9Test.java
+++ src/test/java/com/advent/Exercise9Test.java
@@ -13,28 +13,28 @@ class Exercise9Test {
     void fileSystemCheckSumTest() throws IOException {
         //var diskMap = "2333133121414131402";
         var diskMap = Files.readString(Path.of("src/test/resources/exercise9/disk-map.txt"));
-        var result = Exercise9b.fileSystemCheckSum(diskMap);
+        var result = Exercise9.fileSystemCheckSum(diskMap);
         Assertions.assertEquals(1928, result);
     }
 
     @Test
     void fileSystemCheckSumRange1Test() {
         var diskMap = "233313312141413140211";
-        var result = Exercise9b.fileSystemCheckSum(diskMap);
-        Assertions.assertEquals(4024, result);
+        var result = Exercise9.fileSystemCheckSum(diskMap);
+        Assertions.assertEquals(2132, result);
     }
 
     @Test
     void fileSystemCheckSumRange2Test() {
         var diskMap = "2333133121414131402332233";
-        var result = Exercise9b.fileSystemCheckSum(diskMap);
+        var result = Exercise9.fileSystemCheckSum(diskMap);
         Assertions.assertEquals(4024, result);
     }
 
     @Test
     void fileSystemCheckSumLargeTest() throws IOException {
         var diskMap = Files.readString(Path.of("src/test/resources/exercise9/disk-map-large.txt"));
-        var result = Exercise9b.fileSystemCheckSum(diskMap);
+        var result = Exercise9.fileSystemCheckSum(diskMap);
         System.out.println("CheckSum: " + result);
         //CheckSum: 181189696 That's not the right answer; your answer is too low
         //CheckSum: 282055995 That's not the right answer; your answer is too low.
